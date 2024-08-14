8496355481b368e334c0e0716a33a9b6f5ac8bf6
dongjingpan <dongjingpan@autox.ai>
2024-08-14 17:59:48 +0800

    opt

--- setup.sh
+++ setup.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-dst_dir="/usr/local/bin"
+dst_dir="/usr/local/frp"
+mkdir -p $dst_dir
 
 service_type=$1
 
-sudo cp $service_type $dst_dir
+sudo cp -rf $service_type $dst_dir
 
-cp -f $service_type/frpc.service /etc/systemd/system
+cp -f $service_type/$service_type.service /etc/systemd/system
 systemctl daemon-reload
 systemctl enable $service_type
 systemctl start $service_type
