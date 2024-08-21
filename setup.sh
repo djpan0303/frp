58d4b39bb03b1b82dca0cb7fef0f0ab17428f9b3
dongjingpan <dongjingpan@autox.ai>
2024-08-21 10:23:41 +0800

    sudo privillege

--- setup.sh
+++ setup.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+set -x
+
 service_type=$1
 if [ -z "$service_type" ];then
     echo "service_type is null. can be 'frpc' or 'frps'"
@@ -8,11 +11,11 @@ fi
 echo "service_type is $service_type"
 
 dst_dir="/usr/local/frp"
-mkdir -p $dst_dir
+sudo mkdir -p $dst_dir
 
 sudo cp -rf $service_type $dst_dir
 
 cp -f $service_type/$service_type.service /etc/systemd/system
-systemctl daemon-reload
-systemctl enable $service_type
-systemctl start $service_type
+sudo systemctl daemon-reload
+sudo systemctl enable $service_type
+sudo systemctl start $service_type
